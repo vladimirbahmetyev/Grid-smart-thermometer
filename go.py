fc87dd171fdf1b22189296265c94417f4ba8c1ce
Anastasiya Bartashuk <55900056+ananassta@users.noreply.github.com>
2021-04-19 18:33:59 +0300

    Added some work with arduino conection

--- go.py
+++ go.py
@@ -1,10 +1,10 @@
-import serial  # подключаем библиотеку для последовательной связи
-import time  # подключаем библиотеку чтобы задействовать функции задержки в программе
+import serial  # connect the library for serial communication
+import time  # connect the library to use the delay functions in the program
 from imutils.video import VideoStream
 import numpy as np
 import imutils
 import time
-import cv2
+import cv2 as cv2
 
 
 def check_person(camera_stream):
@@ -14,7 +14,7 @@ def check_person(camera_stream):
                "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
                "sofa", "train", "tvmonitor"]
     # Loading model
-    net = cv2.dnn.readNetFromCaffe('MobileNetSSD_deploy.prototxt.txt', 'MobileNetSSD_deploy.caffemodel')
+    net = cv2.cv2.dnn.readNetFromCaffe('MobileNetSSD_deploy.prototxt.txt', 'MobileNetSSD_deploy.caffemodel')
     # Getting frame by a camera
     frame = camera_stream.read()
     if frame is None:
@@ -23,7 +23,7 @@ def check_person(camera_stream):
     # Frame's processing
     frame = imutils.resize(frame, width=400)
     (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+    blob = cv2.cv2.dnn.blobFromImage(cv2.cv2.resize(frame, (300, 300)),
                                  0.007843, (300, 300), 127.5)
     net.setInput(blob)
     detections = net.forward()
@@ -43,21 +43,27 @@ def check_person(camera_stream):
     return '0'
 
 
-# def start_mesuring(pusk):
-ArduinoSerial = serial.Serial('com3', 9600)  # создаем объект для работы с портом последовательной связи
-# time.sleep(2)  # ждем 2 секунды чтобы установилась последовательная связь
-# print (ArduinoSerial.readline()) #считываем данные из последовательного порта и печатаем их в виде строки
+
+ArduinoSerial = serial.Serial('com3', 9600)  # create an object to work with a serial communication port
+time.sleep(2)  # wait 2 seconds for serial communication to be established
+# print (ArduinoSerial.readline()) # read data from the serial port and print it as a string
 # print("Enter 1 to turn ON LED and 0 to turn OFF LED")
 camera_stream = VideoStream(src=0).start()
 while True:
     is_person_detected = check_person(camera_stream)
     if is_person_detected == '1':
         print('Human detected')
+        ArduinoSerial.write(is_person_detected.encode())  # pass 1
+        check = str(ArduinoSerial.readline()) 
+        a = str(b'1\r\n')
+        while (check != a):
+            check = str(ArduinoSerial.readline())
+            print (check)
     else:
         print('Human not detected')
-    ArduinoSerial.write(is_person_detected.encode())  # передаем 1
+        ArduinoSerial.write(is_person_detected.encode())  # pass 0
     # print ("LED turned ON")
     # print (ArduinoSerial.readline())
     time.sleep(1)
-    # time.sleep(15)
+    #time.sleep(15)
     # print (ArduinoSerial.readline())
